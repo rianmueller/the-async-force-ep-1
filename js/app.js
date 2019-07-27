dc667c0b6c374beb3545ab745efeaf9b2ea938ab
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-07-26 16:57:52 -1000

    resolves person 14 name and species

--- js/app.js
+++ js/app.js
@@ -1,24 +1,32 @@
-// document.getElementById("person4Name").innerHTML = "";
-// document.getElementById("person4HomeWorld").innerHTML = "";
-// document.getElementById("person14Name").innerHTML = "";
-// document.getElementById("person14Species").innerHTML = "";
-
 const person4Request = new XMLHttpRequest();
-person4Request.addEventListener("load", person4RequestListener);
-person4Request.open("GET", "https://swapi.co/api/people/4/");
-person4Request.send();
-
-function person4RequestListener() {
+person4Request.addEventListener("load", function() {
   person4Object = JSON.parse(this.responseText);
   document.getElementById("person4Name").innerHTML = person4Object.name;
 
   const homeWorldRequest = new XMLHttpRequest();
-  homeWorldRequest.addEventListener("load", homeWorldRequestListener);
+  homeWorldRequest.addEventListener("load", function() {
+    homeWorldObject = JSON.parse(this.responseText);
+    document.getElementById("person4HomeWorld").innerHTML =
+      homeWorldObject.name;
+  });
   homeWorldRequest.open("GET", person4Object.homeworld);
   homeWorldRequest.send();
-}
+});
+person4Request.open("GET", "https://swapi.co/api/people/4/");
+person4Request.send();
 
-function homeWorldRequestListener() {
-  homeWorldObject = JSON.parse(this.responseText);
-  document.getElementById("person4HomeWorld").innerHTML = homeWorldObject.name;
-}
+const person14Request = new XMLHttpRequest();
+person14Request.addEventListener("load", function() {
+  person14Object = JSON.parse(this.responseText);
+  document.getElementById("person14Name").innerHTML = person14Object.name;
+
+  const speciesRequest = new XMLHttpRequest();
+  speciesRequest.addEventListener("load", function() {
+    speciesObject = JSON.parse(this.responseText);
+    document.getElementById("person14Species").innerHTML = speciesObject.name;
+  });
+  speciesRequest.open("GET", person14Object.species);
+  speciesRequest.send();
+});
+person14Request.open("GET", "https://swapi.co/api/people/14/");
+person14Request.send();
